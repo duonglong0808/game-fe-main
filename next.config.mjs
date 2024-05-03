cecefc83c85ec1d8fb1537dfc75f4338435b4a79
duonglong0808 <165501031+duonglong0808@users.noreply.github.com>
2024-05-03 22:11:39 +0700

    update env

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,15 @@ const nextConfig = {
     URL_GAME: 'https://game.vk169.net',
     HOST_KU: 'https://vn.vc3559k.net/api',
   },
+
+  // Production environment
+  // env: {
+  //   API_URL: 'https://api.vk169.net/api',
+  //   API_URL_DICE: 'https://game.api.vk169.net/api',
+  //   API_URL_WSK: 'https://game.wsk.vk169.net',
+  //   URL_GAME: 'https://game.vk169.net',
+  //   HOST_KU: 'https://vn.vc3559k.net/api',
+  // },
 };
 
 export default nextConfig;

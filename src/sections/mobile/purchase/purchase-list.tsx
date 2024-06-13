2026143e0c9e9f865fbe3a36c81825738f3ad15c
duonglong0808 <165501031+duonglong0808@users.noreply.github.com>
2024-06-13 20:56:23 +0700

    udpat

--- src/sections/mobile/purchase/purchase-list.tsx
+++ src/sections/mobile/purchase/purchase-list.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { PURCHASE } from '@/_mock/_purchase';
 import { usePaymentTypes } from '../utils/handleMember';
 import { paths } from '@/routes/paths';
 import { Status } from '@/constant';
